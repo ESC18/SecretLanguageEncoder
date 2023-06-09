0b4deccf2ae1a9860887d6940279d5cda55eb7dc
ESC18 <ponderosaprintingus@gmail.com>
2023-06-09 09:42:24 -0700

    make site responsive, and fix submit failure.

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
 window.onload = function () {
     let form = document.getElementById("form");
-    let box = document.getElementById("Password").value;
+    form.addEventListener("submit", submitFunction);
+}
+let submitFunction = function (event) {
+    event.preventDefault();
+    let test = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3","4","5","6","7","8","9","0",".",",","?","/","<",">","|","[","]","{","}",";",":","'",'"',"~","`","!","@","#","$","%","^","&","*","(",")","-","_","+","=","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    let test2 = ["π","∞","Σ","√","∫","∮","∀","∁","∂","∅","∆","∈","∎","∏","∑","∓","∘","∟","∡","∤","∴","≣","⊠","⊙","⋙","≹",/*numbers*/"◍","❂","⊪","☢","〶","╟","╝","┝","╥","⊮",/*symbols*/"⍋","﹝","⌧","⍙","⌬","⌮","⌗","⍞","⍥","⌘","⌆","⍵","⌤","⌨","⍹","⌸","␢","⌯","⌝","⌂","⌔","⌌","⍟","⌭","⍠","⍦","⍪","⍭","❏","▩","☵",/*uppercase*/"↭","➫","⬲","➙","↟","⟿","⥑","➽","⟱","⥼","⥉","⥤","☍","⭀","➤","↩","➴","⬲","⤼","⟺","⭇","➠","☊","↹","↡","⇹"];
     let div = document.getElementById("p");
     let div2 = document.getElementById("p2");
-    let button = document.getElementById("thebutton");
-    let button2 = document.getElementById("thebutton2");
-    let test = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", "!", "?"];
-    let test2 = ["1" ,"2" ,"3" ,"4" ,"5" ,"6" ,"7" ,"8" ,"9" ,"0" ,"~" ,"_" ,"@" ,"#" ,"$" ,"%" ,"^" ,"&" ,"*" ,"(" ,")" ,"[" ,"]" ,"{" ,"}" ,"=", "|", ":", "+"];
-    button.addEventListener("touchstart", submitFunction);
-    button2.addEventListener("touchstart", refreshFunction);
-    let inputText = box;
-    let savedText = inputText;
-    let savedText2 = inputText;
-    function submitFunction(event) {
-        event.preventDefault();
-        for (index = 0; index < test.length; index++) {
-                savedText = savedText.replaceAll(test[index],test2[index]);
-                div.innerHTML = savedText;
-                savedText2 = savedText2.replaceAll(test2[index],test[index]);
-                div2.innerHTML = savedText2;
-            }
-            
-        } 
-    function refreshFunction() {
-        location.reload();
-        
+    let box = document.getElementById("Password").value;
+    let savedText = box;
+    let savedTextTwo = box;
+    for (index = 0; index < test.length; index++) {
+        savedText = savedText.replaceAll(test[index], test2[index]);
+        div.innerText = savedText;
+        savedTextTwo = savedTextTwo.replaceAll(test2[index], test[index]);
+        div2.innerHTML = savedTextTwo;
+        form.reset();
     }
 }
-
