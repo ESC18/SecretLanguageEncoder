6fd24b9c181368ace8e734529a4a61be4a0a770d
ESC18 <ponderosaprintingus@gmail.com>
2023-06-08 23:24:12 -0700

    fix inability to refresh v3.

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ window.onload = function () {
     let button2 = document.getElementById("thebutton2");
     let test = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", "!", "?"];
     let test2 = ["1" ,"2" ,"3" ,"4" ,"5" ,"6" ,"7" ,"8" ,"9" ,"0" ,"~" ,"_" ,"@" ,"#" ,"$" ,"%" ,"^" ,"&" ,"*" ,"(" ,")" ,"[" ,"]" ,"{" ,"}" ,"=", "|", ":", "+"];
-    button.addEventListener("click", submitFunction);
-    button2.addEventListener("click", refreshFunction);
+    button.addEventListener("touchstart", submitFunction);
+    button2.addEventListener("touchstart", refreshFunction);
     let inputText = box;
     let savedText = inputText;
     let savedText2 = inputText;
